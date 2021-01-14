0c8b456ef84eae6ea181537f9908e3c8aece1b0b
AlexTamagnini <Alex.tamagnini2002@gmail.com>
2021-01-14 17:57:05 +0100

    InfoTorneo

--- test/test/InfoTorneo.aspx.designer.cs
+++ test/test/InfoTorneo.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace test
         /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label idricevuto;
+
+        /// <summary>
+        /// Controllo torneiInfo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder torneiInfo;
     }
 }
