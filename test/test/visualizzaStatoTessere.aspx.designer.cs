68cbf99e6401afb29095f435d970fd8d6ba145d0
Filippo Vandi <filippovandi01@gmail.com>
2021-02-10 12:37:49 +0100

    visualizza stato tessere dei componenti società
    da completare (manca l'api)

---
+++ test/test/visualizzaStatoTessere.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace test
+{
+
+
+    public partial class visualizzaStatoTessere
+    {
+
+        /// <summary>
+        /// Controllo formComponentiSocieta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm formComponentiSocieta;
+
+        /// <summary>
+        /// Controllo lblTessere.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTessere;
+
+        /// <summary>
+        /// Controllo visualizzaTessere.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder visualizzaTessere;
+    }
+}
