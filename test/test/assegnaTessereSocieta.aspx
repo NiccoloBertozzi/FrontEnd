101638ca2b9df0bb5c617f1e2e93862f3dc313d6
Filippo Vandi <filippovandi01@gmail.com>
2021-02-12 10:29:59 +0100

    creazione form assegna tessere

---
+++ test/test/assegnaTessereSocieta.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="assegnaTessereSocieta.aspx.cs" Inherits="test.assegnaTessereSocieta" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" text="Assegnazione Tessere"></asp:Label>
+            
+            <asp:Label runat="server" ID="lblIdMembro" Text="Id membro: "></asp:Label> <!-- da modificare -->
+            <asp:TextBox runat="server" ID="txtIdMembri"></asp:TextBox> <br />
+
+            <asp:Label runat="server" ID="lblCodiceTessera" Text="Codice tessera: "></asp:Label>
+            <asp:TextBox runat="server" ID="txtCodiceTessera"></asp:TextBox> <br />
+
+            <asp:Label runat="server" ID="lblTipoTessera" Text="Tipo tessera: "></asp:Label>
+            <asp:TextBox runat="server" ID="txtTipoTessera"></asp:TextBox> <br />
+
+            <asp:Label runat="server" ID="lblDataTesseramento" Text="Data di tesseramento: "></asp:Label>
+            <asp:TextBox runat="server" ID="txtDataTesseramento"></asp:TextBox> <br />
+
+            <asp:Label runat="server" ID="lblAnnoTesseramento" Text="Anno di tesseramento: "></asp:Label>
+            <asp:TextBox runat="server" ID="txtAnnoTesseramento"></asp:TextBox> <br />
+
+            <asp:Button runat="server" ID="btnInvio" Text="invio" OnClick="btnInvio_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ test/test/assegnaTessereSocieta.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generato automaticamente>
+//     Codice generato da uno strumento.
+//
+//     Le modifiche a questo file possono causare un comportamento non corretto e verranno perse se
+//     il codice viene rigenerato. 
+// </generato automaticamente>
+//------------------------------------------------------------------------------
+
+namespace test
+{
+
+
+    public partial class assegnaTessereSocieta
+    {
+
+        /// <summary>
+        /// Controllo form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controllo lblIdMembro.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblIdMembro;
+
+        /// <summary>
+        /// Controllo txtIdMembri.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIdMembri;
+
+        /// <summary>
+        /// Controllo lblCodiceTessera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCodiceTessera;
+
+        /// <summary>
+        /// Controllo txtCodiceTessera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCodiceTessera;
+
+        /// <summary>
+        /// Controllo lblTipoTessera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTipoTessera;
+
+        /// <summary>
+        /// Controllo txtTipoTessera.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTipoTessera;
+
+        /// <summary>
+        /// Controllo lblDataTesseramento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDataTesseramento;
+
+        /// <summary>
+        /// Controllo txtDataTesseramento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDataTesseramento;
+
+        /// <summary>
+        /// Controllo lblAnnoTesseramento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblAnnoTesseramento;
+
+        /// <summary>
+        /// Controllo txtAnnoTesseramento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtAnnoTesseramento;
+
+        /// <summary>
+        /// Controllo btnInvio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generato automaticamente.
+        /// Per la modifica, spostare la dichiarazione di campo dal file di progettazione al file code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInvio;
+    }
+}
