5f3e48990594083c7ea75eb061dc90337107945c
AlexTamagnini <Alex.tamagnini2002@gmail.com>
2021-02-24 12:56:05 +0100

    Gestione problemi login per la seconda volta

--- test/test/OutputClassifica.aspx.cs
+++ test/test/OutputClassifica.aspx.cs
@@ -15,7 +15,50 @@ namespace test
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["ruolo"] != null)
+            {
+                if (Session["ruolo"].ToString() == "Atleta")
+                {
+                    StringBuilder table = new StringBuilder();
+                    table.Clear();
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadPageIscritti(); \">Tornei Iscritti</a></li>");
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                }
+                else if (Session["ruolo"].ToString() == "Admin")
+                {
+                    StringBuilder table = new StringBuilder();
+                    table.Clear();
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadPage(); \">Non autorizzati</a></li>");
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadPageDelegati(); \">Tornei delegato</a></li>");
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                }
+                else if (Session["ruolo"].ToString() == "Societa")
+                {
+                    StringBuilder table = new StringBuilder();
+                    table.Clear();
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadCreaTorneo(); \">Crea Torneo</a></li>");
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                }
+                else if (Session["ruolo"].ToString() == "Delegato")
+                {
+                    StringBuilder table = new StringBuilder();
+                    table.Clear();
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadPageDelegati(); \">Tornei delegato</a></li>");
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                }
+                else if (Session["ruolo"].ToString() == "Allenatore")
+                {
+                    StringBuilder table = new StringBuilder();
+                    table.Clear();
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                    table.Append("<li class=\"nav-item\" role=\"presentation\"><a class=\"nav-link active pointer\" onclick=\"LoadPageIscritti(); \">Tornei Iscritti</a></li>");
+                    dinamicload.Controls.Add(new Literal { Text = table.ToString() });
+                }
+            }
         }
     }
 }
